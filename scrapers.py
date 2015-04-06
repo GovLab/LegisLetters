845436b02f20aa21d3d0f0ce08ba5b07ad4d8d0d
John Krauss <irving.krauss@gmail.com>
2015-04-06 10:41:34 -0400

    pylint/pyflakes compliance

--- scrapers.py
+++ scrapers.py
@@ -1,64 +1,93 @@
+'''
+docstring
+'''
+
 import urllib2
 import urllib
-from BeautifulSoup import BeautifulSoup
-import simplejson
+from bs4 import BeautifulSoup
 import logging
 import sys
 
-def fetchPage(url):
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(logging.StreamHandler(sys.stderr))
+
+def fetch_page(url):
+    '''
+    get page with urllib2, return text response
+    '''
     request = urllib2.Request(url)
-    request.add_header('User-Agent', 
+    request.add_header('User-Agent',
                        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.19) '
                        'Gecko/20081202 Firefox (Debian-2.0.0.19-0etch1)')
     opener = urllib2.build_opener(urllib2.HTTPRedirectHandler())
     return opener.open(request)
 
-def scrapeGoogle(query, site, start=0):
+
+def scrape_google(query, site, start=0):
+    '''
+    Scrape a query from google for a specific site.
+    '''
     entity = urllib.quote(query)
     site_restrict = urllib.quote('site:%s' % site)
-    URL = "http://www.google.com/search?q=%s+%s&start=%d" % (entity, site_restrict, start)
-    print URL
-    logging.info("Processing %s" % URL)
+    url = "http://www.google.com/search?q=%s+%s&start=%d" % (entity, site_restrict, start)
+    LOGGER.info("Processing %s", url)
     results = []
-    response = fetchPage(URL)
+    response = fetch_page(url)
     soup = BeautifulSoup(response)
-    results.extend([ processUrlFromGoogle(t.a['href']) for t in soup.findAll('h3', attrs={'class': 'r'}) ])
+    results.extend([
+        process_url_from_google(t.a['href']) for t in soup.findAll('h3', attrs={'class': 'r'})])
     return results
 
-def processUrlFromGoogle(str):
-    return str[7:].split('&')[0]
 
-query = "\"the full text of the letter is below\""
-site = "senate.gov"
-start = 0
-data = []
-for i in range(0, 10):
-    data.extend(scrapeGoogle(query, site, int(10*i)))
+def process_url_from_google(url):
+    '''
+    ?
+    '''
+    return url[7:].split('&')[0]
 
-def processLetter(url):
-    letterPage = fetchPage(url).read()
-    letterSoup = BeautifulSoup(letterPage)
-    t = [k for k in letterSoup.findAll(text='The full text of the letter is below:')][0]
+QUERY = "\"the full text of the letter is below\""
+SITE = "senate.gov"
+START = 0
+DATA = []
+
+
+def process_letter(url):
+    '''
+    load and process a letter from its url
+    '''
+    letter_page = fetch_page(url).read()
+    letter_soup = BeautifulSoup(letter_page)
+    text = [k for k in letter_soup.findAll(text='The full text of the letter is below:')][0]
     acc = []
-    for item in t.findAllNext():
-        if item.name ==  "footer" or (item.name == 'div' and 'class' in item and item['class'] == 'footer'):
+    for item in text.findAllNext():
+        if item.name == "footer" or \
+           (item.name == 'div' and 'class' in item and item['class'] == 'footer'):
             break
         acc.append(item.text)
-    acc = map(lambda x: x.replace('&nbsp;', ' ').strip(), acc)
+    #acc = map(lambda x: x.replace('&nbsp;', ' ').strip(), acc)  #pylint: disable=bad-builtin
+    acc = [x.replace('&nbsp;', ' ').strip() for x in acc]
     acc = [k for k in acc if k != '']
     return "\n".join(acc)
 
-import os
-import codecs
-for p in data:
-    try:
-        f = codecs.open('%s/letters/%s.txt' % ('/Users/sahuguet/Documents/Notebooks', p.split('/')[-1]), 'w+', 'utf-8')
-        f.write(p)
-        f.write('\n')
-        f.write(processLetter(p))
-        f.close()
-        print "++OK: %s" % p
-    except Exception, e:
-        print "--ERR: %s" % p
-        print >> sys.stderr, e
-    sys.stdout.flush()
+
+if __name__ == '__main__':
+
+    import codecs
+    for i in range(0, 10):
+        DATA.extend(scrape_google(QUERY, SITE, int(10*i)))
+
+    for p in DATA:
+        try:
+            #f = codecs.open('%s/letters/%s.txt'
+            #% ('/Users/sahuguet/Documents/Notebooks', p.split('/')[-1]), 'w+',
+            #'utf-8')
+            f = codecs.open('out.txt', 'w+', 'utf-8')
+            f.write(p)
+            f.write('\n')
+            f.write(process_letter(p))
+            f.close()
+            LOGGER.info("++OK: %s", p)
+        except Exception as err:  #pylint: disable=broad-except
+            LOGGER.error("--ERR: %s (%s)", p, err)
+        sys.stdout.flush()
